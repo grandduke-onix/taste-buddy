4c1d3716f04856991cd934297b3bcede69f1380f
grandduke-onix <106667064+grandduke-onix@users.noreply.github.com>
2023-04-17 17:26:46 +0100

    reduced height for nutrition-info

---
+++ src/components/RecipeDetails/NutritionInfo.jsx
@@ -0,0 +1,61 @@
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import React from "react";
+import { themeSettings } from "../../theme";
+import CloseIcon from "@mui/icons-material/Close";
+
+const NutritionInfo = function (props) {
+	const themes = useTheme();
+	const color = themeSettings(themes.palette.mode);
+	const nutrients = props.nutrients;
+	// console.log(nutrients);
+
+	return (
+		<Box
+			width={"400px"}
+			height={"auto"}
+			backgroundColor={color.palette.surface.mainVariant}
+			borderRadius={"28px"}
+			// px={"24px"}
+			position={"absolute"}
+			top={"20%"}
+			left={"40%"}
+			display={!props.openModal && "none"}
+			color={color.palette.onSurface.main}
+			border={`solid ${color.palette.outline.main}`}
+		>
+			<Box display={"flex"} justifyContent={"end"} px={"24px"}>
+				<IconButton onClick={() => props.setOpenModal(!props.openModal)}>
+					<CloseIcon fontSize="large" />
+				</IconButton>
+			</Box>
+			<Box display={"flex"} justifyContent={"space-between"} pt={"15px"} px={"24px"}>
+				<Typography variant="h3">Nutritional Analysis</Typography>
+				<Typography variant="h3">Per Serving</Typography>
+			</Box>
+
+			<Box pt={"30px"} px={"24px"}>
+				<Box display={"flex"} justifyContent={"space-between"}>
+					<Typography variant="h4">serving size</Typography>
+					<Typography variant="h4">1 of {props.servings}</Typography>
+				</Box>
+				<Divider sx={{ pt: "15px", color: color.palette.outline.main }} />
+			</Box>
+
+			<Box height={"500px"} overflow={"auto"}>
+				{nutrients.map(i => (
+					<Box pt={"24px"} px={"24px"} key={`${i}-${i.name}`}>
+						<Box display={"flex"} justifyContent={"space-between"}>
+							<Typography variant="h4">{i.name}</Typography>
+							<Typography variant="h4">
+								{i.amount} {i.unit}
+							</Typography>
+						</Box>
+						<Divider sx={{ pt: "15px", color: color.palette.outline.main }} flexItem />
+					</Box>
+				))}
+			</Box>
+		</Box>
+	);
+};
+
+export default NutritionInfo;
