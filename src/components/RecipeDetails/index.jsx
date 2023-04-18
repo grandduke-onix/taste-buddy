bf70a9397559fc6edca143611320645e14ac96f4
grandduke-onix <106667064+grandduke-onix@users.noreply.github.com>
2023-04-18 15:58:52 +0100

    fixed bug on api call in recipe details

--- src/components/RecipeDetails/index.jsx
+++ src/components/RecipeDetails/index.jsx
@@ -1,20 +1,13 @@
-import { Box, useMediaQuery, useTheme } from "@mui/material";
-import React, { useState } from "react";
-import { useLoaderData, useParams } from "react-router-dom";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { APIKEY } from "../../configure";
 import { themeSettings } from "../../theme";
 import RecipeSummary from "./RecipeSummary";
 import RecipeIngredients from "./RecipeIngredients";
 import NutritionInfo from "./NutritionInfo";
-
-// import { pathToRegexp, match, parse, compile } from "path-to-regexp";
-
-// const bed = window.location;
-// console.log(bed);
-// const regexp = pathToRegexp("/:foo/:bar");
-// // console.log(regexp);
-// const egg = regexp.exec(bed.pathname);
-// console.log(egg);
+import RecipeInstructions from "./RecipeInstructions";
+import { RouteChangeAnimation } from "../Loader";
 
 const RecipeDetails = function () {
 	const isNonMobile = useMediaQuery("(min-width:600px)");
@@ -22,36 +15,80 @@ const RecipeDetails = function () {
 	const color = themeSettings(themes.palette.mode);
 	const [openModal, setOpenModal] = useState(false);
 	const { recipeId } = useParams();
-	// const recipeData = useLoaderData();
-	// console.log(recipeData);
-	console.log(openModal);
+	const [recipeData, setRecipeData] = useState(null);
+
+	const dataLoader = async function () {
+		const response = await fetch(
+			`https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=true&apiKey=${APIKEY}`
+		);
+		const responseJson = await response.json();
+		setRecipeData(responseJson);
+	};
+
+	useEffect(() => {
+		dataLoader();
+	}, [recipeId]);
+
 	return (
-		<Box display={"flex"}>
-			<Box flex={"1 0 60%"} height={"500px"}>
-				<RecipeSummary setOpenModal={setOpenModal} openModal={openModal} />
-			</Box>
-			<Box
-				backgroundColor={color.palette.surface.mainVariant}
-				flex={"1 0 40%"}
-				height={"auto"}
-			>
-				<RecipeIngredients />
-			</Box>
-			<NutritionInfo openModal={openModal} setOpenModal={setOpenModal} />
-			{/* <Box height={"100px"} width={"100px"} backgroundColor={"aqua"}></Box> */}
+		<Box>
+			{/* For lazy loading when changing the route */}
+			{recipeData ? (
+				<Box>
+					<Box display={"flex"}>
+						{/* Right hand side */}
+						<Box flex={"1 0 60%"} height={"auto"}>
+							<RecipeSummary
+								setOpenModal={setOpenModal}
+								openModal={openModal}
+								title={recipeData.title}
+								author={recipeData.sourceName}
+								summary={recipeData.summary}
+								time={recipeData.readyInMinutes}
+								yield={recipeData.servings}
+								PPS={recipeData.pricePerServing}
+								healthScore={recipeData.healthScore}
+								tags={[
+									...recipeData.cuisines,
+									...recipeData.dishTypes,
+									...recipeData.diets,
+									...recipeData.occasions,
+								]}
+							/>
+							<RecipeInstructions
+								instructions={recipeData.analyzedInstructions[0].steps}
+							/>
+						</Box>
+
+						{/* Left hand side */}
+						<Box
+							backgroundColor={color.palette.surface.mainVariant}
+							flex={"1 0 40%"}
+							height={"auto"}
+						>
+							<Box position={"sticky"} top={"3px"}>
+								<RecipeIngredients
+									image={recipeData.image}
+									ingredients={recipeData.extendedIngredients}
+								/>
+							</Box>
+						</Box>
+						<NutritionInfo
+							openModal={openModal}
+							setOpenModal={setOpenModal}
+							servings={recipeData.servings}
+							nutrients={recipeData.nutrition.nutrients}
+						/>
+					</Box>
+
+					<Box pt={"100px"} px={"50px"}>
+						<Typography variant="h3">Related:</Typography>
+					</Box>
+				</Box>
+			) : (
+				<RouteChangeAnimation />
+			)}
 		</Box>
 	);
 };
 
 export default RecipeDetails;
-
-export const loader = async function () {
-	const response = await fetch(
-		`https://api.spoonacular.com/recipes/646515/information?includeNutrition=true&apiKey=${APIKEY}`
-	);
-	if (!response.ok) {
-		throw new Response(JSON.stringify({ message: "Could not fetch recipes" }), { status: 402 });
-	} else {
-		return response;
-	}
-};
